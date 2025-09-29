b054a2f3791b8e3ed48fb190198cdc48b2b7bfd7
DanThuy14 <danthuy104005@gmail.com>
2025-09-29 21:26:47 +0700

    Add files via upload

---
+++ MaHoa_BVN_4.2.py
@@ -0,0 +1,12 @@
+Plaintext = "DinhTruongDanThuy"
+k = 46
+kq = ""
+for ky_tu in Plaintext:
+    if ky_tu.isalpha():
+        base = ord("A") if ky_tu.isupper() else ord("a")
+        ma_hoa = chr((ord(ky_tu) - base + k) % 26 + base)
+        kq = kq + ma_hoa
+    else:
+        kq = kq + ky_tu
+
+print(f"{Plaintext} sau khi mã hóa: {kq}")
